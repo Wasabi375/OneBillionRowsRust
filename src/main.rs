4d892094105b2eddb190b46cdb78083f41dc8af1
Burkhard Mittelbach <wasabi37a@googlemail.com>
2024-03-05 18:19:03 +0100

    Separate file loading from processing
    We are now at 24s :)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{stdout, Read, Write},
+    io::{stdout, Read, Seek, Write},
+    ops::Deref,
     path::{Path, PathBuf},
+    str::{from_utf8, from_utf8_unchecked},
     thread,
 };
 
@@ -31,85 +33,116 @@ struct CityEntry {
     count: usize,
 }
 
-fn produce_lines(in_path: &Path, sender: Sender<Box<str>>) {
-    let mut file = File::open(in_path).expect("could not open input file");
+const BLOCK_SIZE: usize = 4096;
+struct StrBuffer {
+    raw_data: Box<[u8; BLOCK_SIZE]>,
+    size: usize,
+}
 
-    let mut text_data = String::new();
-    file.read_to_string(&mut text_data)
-        .expect("input is not valid utf8");
-    for line in text_data.lines() {
-        sender.send(line.into()).expect("send line failed");
+impl StrBuffer {
+    fn read_from<R: Read + Seek>(read: &mut R) -> Option<Self> {
+        let mut raw_data = Box::new([0u8; BLOCK_SIZE]);
+
+        let full_size = read
+            .read(raw_data.as_mut())
+            .expect("Failed to read TextChunk");
+
+        if full_size == 0 {
+            return None;
+        }
+
+        let raw_data_slice = &raw_data[0..full_size];
+        let last_nl = full_size - 1 - raw_data_slice.iter().rev().position(|&c| c == b'\n')
+            .expect("TextChunk must contain at least 1 nl.
+                    This is an implementation specific requirement and not part of the challenge spec");
+        assert_eq!(raw_data[last_nl], b'\n');
+
+        let str_data_slice = &raw_data[0..=last_nl];
+        assert_eq!(*str_data_slice.last().unwrap(), b'\n');
+
+        from_utf8(str_data_slice).expect("Expected utf8 data");
+        read.seek(std::io::SeekFrom::Current(
+            -((full_size - last_nl - 1) as i64),
+        ))
+        .expect("Seek to after last nl failed");
+
+        Some(StrBuffer {
+            raw_data,
+            size: last_nl + 1,
+        })
     }
 }
 
-fn process_lines(lines: Receiver<Box<str>>) -> HashMap<String, CityEntry> {
+impl Deref for StrBuffer {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        let chunk = &self.raw_data[0..self.size];
+        // Safety: we check that this is a valid utf8 str when we create the TextChunk
+        unsafe { from_utf8_unchecked(chunk) }
+    }
+}
+
+fn produce_text_chunks(in_path: &Path, sender: Sender<StrBuffer>) {
+    let mut file = File::open(in_path).expect("could not open input file");
+
+    loop {
+        match StrBuffer::read_from(&mut file) {
+            Some(chunk) => sender.send(chunk).expect("Failed to send TextChunk"),
+            None => break,
+        }
+    }
+}
+
+fn process_lines(chunks: Receiver<StrBuffer>) -> HashMap<String, CityEntry> {
     let mut result = HashMap::<String, CityEntry>::new();
     loop {
-        let line = match lines.recv() {
-            Ok(line) => line,
+        let chunk = match chunks.recv() {
+            Ok(chunk) => chunk,
             Err(_) => break,
         };
 
-        let mut parts = line.split(';');
-        let city = parts.next().expect("Expected city name");
-        let value: f32 = parts
-            .next()
-            .expect("Expected value")
-            .parse()
-            .expect("expected float value");
+        for line in chunk.lines() {
+            let mut parts = line.split(';');
+            let city = parts.next().expect("Expected city name");
+            let value = &parts.next().expect("Expected value");
+            let value: f32 = value.parse().expect(&format!(
+                "expected float value but got \"{}\" in line\n{}\n",
+                value,
+                line.replace("\n", "\\n")
+            ));
 
-        if !result.contains_key(city) {
-            result.insert(city.to_string(), CityEntry::default());
-        }
+            if !result.contains_key(city) {
+                result.insert(city.to_string(), CityEntry::default());
+            }
 
-        let entry = result.get_mut(city).unwrap();
-        entry.sum += value;
-        entry.count += 1;
-        if entry.max < value {
-            entry.max = value;
-        }
-        if entry.min > value {
-            entry.min = value;
+            let entry = result.get_mut(city).unwrap();
+            entry.sum += value;
+            entry.count += 1;
+            if entry.max < value {
+                entry.max = value;
+            }
+            if entry.min > value {
+                entry.min = value;
+            }
         }
     }
     result
 }
 
 fn calculate<OWrite: Write>(args: Args, mut output: OWrite) {
-    let (line_sender, line_receiver) = bounded(100);
+    let (chunk_sender, chunk_receiver) = bounded(10);
 
-    thread::spawn(move || produce_lines(&args.input, line_sender));
+    thread::spawn(move || produce_text_chunks(&args.input, chunk_sender));
 
     let mut partial_result_handles = Vec::with_capacity(args.threads);
     for _ in 0..args.threads {
-        let line_receiver = line_receiver.clone();
-        let handle = thread::spawn(move || process_lines(line_receiver));
+        let chunk_receiver = chunk_receiver.clone();
+        let handle = thread::spawn(move || process_lines(chunk_receiver));
         partial_result_handles.push(handle);
     }
 
-    let mut result = HashMap::new();
-    for handle in partial_result_handles {
-        let partial = match handle.join() {
-            Ok(p) => p,
-            Err(_) => panic!("process lines failed"),
-        };
-
-        for (p_city, p_data) in partial.into_iter() {
-            result
-                .entry(p_city)
-                .and_modify(|full_data: &mut CityEntry| {
-                    full_data.sum += p_data.sum;
-                    full_data.count += p_data.count;
-                    if full_data.min > p_data.min {
-                        full_data.min = p_data.min;
-                    }
-                    if full_data.max < p_data.max {
-                        full_data.max = p_data.max;
-                    }
-                })
-                .or_insert(p_data);
-        }
-    }
+    let result = combine_results(partial_result_handles);
 
     write!(output, "{{").expect("failed to write output");
 
@@ -136,18 +169,52 @@ fn calculate<OWrite: Write>(args: Args, mut output: OWrite) {
     write!(output, "}}").expect("failed to write output");
 }
 
+fn combine_results(
+    partial_result_handles: Vec<thread::JoinHandle<HashMap<String, CityEntry>>>,
+) -> HashMap<String, CityEntry> {
+    let mut result = HashMap::new();
+    for handle in partial_result_handles {
+        let partial = match handle.join() {
+            Ok(p) => p,
+            Err(_) => panic!("process lines failed"),
+        };
+
+        for (p_city, p_data) in partial.into_iter() {
+            result
+                .entry(p_city)
+                .and_modify(|full_data: &mut CityEntry| {
+                    full_data.sum += p_data.sum;
+                    full_data.count += p_data.count;
+                    if full_data.min > p_data.min {
+                        full_data.min = p_data.min;
+                    }
+                    if full_data.max < p_data.max {
+                        full_data.max = p_data.max;
+                    }
+                })
+                .or_insert(p_data);
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
-    use std::{fs::File, io::Read, path::PathBuf};
+    use std::{
+        fs::File,
+        io::{Read, Seek},
+        path::PathBuf,
+        str::from_utf8,
+    };
 
-    use crate::{calculate, Args};
+    use crate::{calculate, Args, StrBuffer};
 
     fn check(in_path: PathBuf, expected_path: PathBuf) {
         let mut result = Vec::new();
 
         let args = Args {
             input: in_path,
-            threads: 4,
+            threads: 1,
         };
 
         calculate(args, &mut result);
@@ -156,7 +223,7 @@ mod test {
         let mut file = File::open(expected_path).unwrap();
         file.read_to_end(&mut expected).unwrap();
 
-        assert_eq!(expected, result);
+        assert_eq!(from_utf8(&expected).unwrap(), from_utf8(&result).unwrap());
     }
 
     #[test]
@@ -183,5 +250,34 @@ mod test {
             PathBuf::from("data/cities_400.txt"),
             PathBuf::from("data/cities_400_res.txt"),
         );
+    }
+
+    #[test]
+    fn check_read_str_buffer() {
+        let mut file =
+            File::open(PathBuf::from("data/test.txt")).expect("could not open input file");
+        let mut full_data = String::new();
+        loop {
+            match StrBuffer::read_from(&mut file) {
+                Some(chunk) => full_data.push_str(&chunk),
+                None => break,
+            }
+        }
+
+        file.seek(std::io::SeekFrom::Start(0)).unwrap();
+        let mut expected = String::new();
+        file.read_to_string(&mut expected).unwrap();
+
+        assert_eq!(expected, full_data);
+    }
+
+    #[test]
+    fn check_read_single_str_buffer() {
+        let mut file =
+            File::open(PathBuf::from("data/all_cities.txt")).expect("could not open input file");
+        let _chunk = StrBuffer::read_from(&mut file).unwrap();
+        let mut b = [0u8];
+        file.read(&mut b).unwrap();
+        assert_ne!(b[0], b'\n');
     }
 }
